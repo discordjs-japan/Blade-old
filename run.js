ad724dd42563a590d5937569b94a471ec53526dd
InkoHX <33122816+InkoHX@users.noreply.github.com>
2018-01-15 16:55:01 +0900

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,19 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const prefix = "./"
+client.on("ready", () => {
+  client.user.setGame('./help');
+  console.log("起動完了\n\nこのBOTはDJS-JPNによって開発されました。");
+});
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command === "help") {
+    message.channel.sendMessage("制作途中");
+  }
+});
 client.login(config.token);
