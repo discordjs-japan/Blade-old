7ca6051b7929b2b3ec5d5a5058fdea194ce58d81
InkoHX <33122816+InkoHX@users.noreply.github.com>
2018-01-23 14:14:12 +0900

    Update npm-install.bat

--- npm-install.bat
+++ npm-install.bat
@@ -1,3 +1,3 @@
 @echo off
 title install
-npm install -g goo.gl & npm install --save discord.js & npm install discord.js node-opus --save & npm install sodium --save & npm install uws --save & npm install --save ytdl-core
+npm install -g goo.gl & npm install discord.js node-opus --save & npm install sodium --save & npm install uws --save & npm install --save ytdl-core
