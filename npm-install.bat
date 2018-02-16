dd7de1fdf1be3f3372b645c2750f9f53d243099d
NULL Coding <33650024+NULL-Coding@users.noreply.github.com>
2018-02-16 18:44:09 +0900

    Update npm-install.bat

--- npm-install.bat
+++ npm-install.bat
@@ -1,3 +1,8 @@
 @echo off
 title install
-npm install discord.js node-opus --save & npm install sodium --save & npm install uws --save
+npm install discord.js
+npm install node-opus
+npm install sodium
+npm install request
+npm install fs
+npm install fs-extra
