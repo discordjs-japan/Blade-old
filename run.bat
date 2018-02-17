006a9876ba249ad6affba04fb4e72c3e6a375953
yuta0801 <yuta20030801@icloud.com>
2018-02-17 15:59:12 +0900

    起動時にインストール、バージョンチャックを実行

--- run.bat
+++ run.bat
@@ -1,3 +1,38 @@
 @echo off
 title BladeBOT
-node run.js
+
+node -v >nul 2>&1
+
+if %errorlevel% == 0 goto exists
+
+echo Node.jsがインストールされていません。
+echo 以下からNode.jsをダウンロードし、インストールしてください。
+echo https://nodejs.org/ja/
+echo.
+pause
+exit
+
+:exists
+
+for /f "usebackq delims=" %%a in (`node -v`) do set NODE=%%a
+
+set "VER=%NODE:.=%"
+set "VER=%VER:v=%"
+
+if %VER% geq 800 goto run
+
+echo このボットの推奨バージョンはv8.0.0以降ですが、
+echo インストールされているnodeのバージョンは%NODE%です
+echo 続行すると不具合が発生する可能性があります。
+
+choice /m "続行しますか"
+
+if /i "%USER_UPDATE%"=="y" (goto run)
+
+exit
+
+:run
+
+node ./run.js
+
+pause
--- run.sh
+++ run.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
+type "node" >/dev/null 2>&1
 
-$ node run.js
+if [ $? != 0 ]; then
+  echo "Node.jsがインストールされていません。"
+  echo "以下からNode.jsをダウンロードし、インストールしてください。"
+  echo "https://nodejs.org/ja/"
+  exit
+fi
+
+NODE=`node -v`
+dpkg --compare-versions "8.0.0" "gt" ${NODE#v}
+if [ $? -eq "0" ]; then
+  echo "このボットの推奨バージョンはv8.0.0以降ですが、"
+  echo "インストールされているnodeのバージョンは%NODE%です"
+  echo "続行すると不具合が発生する可能性があります。"
+  echo -n "続行しますか？ [y/N]"
+  read ANSWER
+
+  case $ANSWER in
+    "Y" | "y") echo;;
+    * ) exit;;
+  esac
+fi
+
+node ./run.js
