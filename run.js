b4eb265ed4e65d2929e4c3fe4ddde8c9bdaf66ce
InkoHX <inkohx@gmail.com>
2018-04-28 13:35:14 +0900

    nameからidへ

--- run.js
+++ run.js
@@ -219,7 +219,7 @@ client
         .setFooter('DEVELOPED BY DJS-JPN', 'https://avatars3.githubusercontent.com/u/35397294?s=200&v=4')
         .setThumbnail(member.user.avatarURL)
         .setColor('#FFFFFF')
-      client.channels.get('name', Config.WelcomeChannel).send(Embed)
+      client.channels.get('id', Config.WelcomeChannel).send(Embed)
     } else {
       const Embed = new DiscordJS.RichEmbed()
         .addField('新しいボットがサーバーに参加しました。', `参加したボット：${member.user.tag}`, true)
@@ -229,7 +229,7 @@ client
         .setFooter('DEVELOPED BY DJS-JPN', 'https://avatars3.githubusercontent.com/u/35397294?s=200&v=4')
         .setThumbnail(member.user.avatarURL)
         .setColor('#FFFFFF')
-      client.channels.get('name', Config.WelcomeChannel).send(Embed)
+      client.channels.get('id', Config.WelcomeChannel).send(Embed)
     }
   })
   .on('guildMemberRemove', member => {
@@ -243,7 +243,7 @@ client
         .setFooter('DEVELOPED BY DJS-JPN', 'https://avatars3.githubusercontent.com/u/35397294?s=200&v=4')
         .setThumbnail(member.user.avatarURL)
         .setColor('#0x00FF00')
-      client.channels.get('name', Config.WelcomeChannel).send(Embed)
+      client.channels.get('id', Config.WelcomeChannel).send(Embed)
     } else {
       const Embed = new DiscordJS.RichEmbed()
         .addField('ボットがサーバーから退出しました。', `退出したボット：${member.user.tag}`, true)
@@ -253,7 +253,7 @@ client
         .setFooter('DEVELOPED BY DJS-JPN', 'https://avatars3.githubusercontent.com/u/35397294?s=200&v=4')
         .setThumbnail(member.user.avatarURL)
         .setColor('#0x00FF00')
-      client.channels.get('name', Config.WelcomeChannel).send(Embed)
+      client.channels.get('id', Config.WelcomeChannel).send(Embed)
     }
   })
 
